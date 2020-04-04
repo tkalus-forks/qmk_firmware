753e453ea824658ec398c9f3e7fabeddbabec6cf
Turtle Kalus <turtle@kalus.us>
2020-04-04 13:38:27 -0700

    [Keymap] Add tkalus mapping to DZ60
    Mapping for a 61% keyboard (3x1.25 + 6sp + 4x1.25).
    - macos CMD/Alt positioning.
    - Combo Ctrl/Esc at traditional Caps Lock.
    - Dual FN keys activating a movement layer.
      Gamer (WASD) and vim-ish (hjkl) friendly arrow-movement
    - FN-Grave activating media, LED, and Screen Brightness Layer
    - FN-Bksp to lock/sleep macos.
    
    Reduced LED Animations.
    
    TODO: Alt-FN ordering to activate Layer 2; currently needs FN-Alt.

---
+++ keyboards/dz60/keymaps/tkalus/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define PREVENT_STUCK_MODIFIERS
+
+#define RGBLIGHT_SLEEP
+
+// Disable `all` animations and enable only desired to save memory.
+#undef RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
